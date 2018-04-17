f8ef189f991d881d0f54cb1b6f49084df063c5a9
smulvey2 <33791425+smulvey2@users.noreply.github.com>
2018-04-16 21:52:39 -0500

    Update GraphProcessor.java

--- GraphProcessor.java
+++ GraphProcessor.java
@@ -1,3 +1,22 @@
+////////////////////////////////////////////////////////////////////////////
+//Semester:         CS400 Spring 2018
+//PROJECT:          P4 Dictionary Graph
+//FILES:            Graph.java
+//GraphProcessor.java
+//GraphTest.java
+//WordProcessor.java
+//
+//USER:         Steven Mulvey
+//				Brandon Jonen	
+//				Natalie Brooks
+//				Nick Merfield
+//				Michael O'Connor
+//
+//Instructor:       Deb Deppeler (deppeler@cs.wisc.edu)
+//Bugs:             no known bugs
+//
+//Due:				2018 Apr 16, 2018 GraphProcessor.java 
+////////////////////////////80 columns wide //////////////////////////////////
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
